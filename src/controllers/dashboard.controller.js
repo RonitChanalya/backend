92b69a8c2fb1fdbcd873798b2398d0f86818f2ca
Ronit Chanalya <ronitchanalya2005@gmail.com>
2025-07-13 22:12:26 +0530

    controlllers done

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -3,6 +3,7 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { Video } from "../models/video.model.js"
 import { isValidObjectId } from "mongoose"
+import { Subscription } from "../models/subscription.model.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     /*
@@ -12,19 +13,11 @@ const getChannelStats = asyncHandler(async (req, res) => {
             3. Total views
             4. Total likes
             5. Total subscribed channels
-    */
 
-    const { channelId } = req.params
-
-    if(!channelId || !isValidObjectId(channelId)) {
-        throw new ApiError(401, "Invalid or missing channel id")
-    }
-
-    /* 
-    1. Total Subscribers: ✅
+    1. Total Subscribers: 
         To get the total subscribers I need to count the total number of documents of scubsciption model in which channel: channelId
 
-    2. Total Videos Uploaded: ✅
+    2. Total Videos Uploaded: 
         I need to count the total number of documents of video model in which owner: channeId
     
     3. Total views: 
@@ -33,71 +26,75 @@ const getChannelStats = asyncHandler(async (req, res) => {
     4. Total Likes:
         I need to sum the count of like model in which video field's owner is channelId
 
-    5. Total subscribed channel: ✅
+    5. Total subscribed channel: 
         I need to count the total number of documents of scubsciption model in which subscriber: channelId
     */
 
-    const channelDetails = await User.aggregate([
+    const { channelId } = req.params;
+    
+    if(!channelId || !isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid or missing Channel Id");
+    }
+
+    const totalSubscribers = await Subscription.countDocuments({ channel: channelId });
+
+    const totalVideos = await Video.countDocuments({ owner: channelId });
+
+    const totalViews = await Video.aggregate([
         {
             $match: {
-                _id: new mongoose.Types.ObjectId(channelId)
+                owner: new mongoose.Types.ObjectId(channelId)
             }
         },
         {
-            $lookup: {
-                from: "subscriptions",
-                localField: "_id",
-                foreignField: "channel",
-                as: "subscribers"
+            $group: { // What does group do
+                _id: null,
+                totalViews: {
+                    $sum: "$views"
+                }
             }
-        },
-        {
-            $lookup: {
-                from: "subscriptions",
-                localField: "_id",
-                foreignField: "subscriber",
-                as: "subscribedTo"
-            }
-        },
+        }
+    ]);
+
+    const totalLikes = await Like.aggregate([
         {
             $lookup: {
                 from: "videos",
-                localField: "_id",
-                foreignField: "owner",
-                as: "totalVideos"
+                localField: "video",
+                foreignField: "_id",
+                as: "videoDoc"
             }
         },
         {
-            $addFields: {
-                subscribersCount: {
-                    $size: "$subscribers"
-                },
-                subscribedChannelsCount: {
-                    $size: "$subscribedTo"
-                },
-                videoCount: {
-                    $size: "$totalVideos"
-                }
+            $unwind: "$videoDoc"
+        },
+        {
+            $match: {
+                "videoDoc.owner": new mongoose.Types.ObjectId(channelId)
             }
         },
         {
-            $lookup: {
-                from: "likes",
-                let: { userId: "$_id" },
-                pipeline: [
-                    {
-                        $lookup: {
-                            from: "videos",
-                            localField: "video",
-                            foreignField: "_id",
-                            as: "videoDoc"
-                        }
-                    }
-                ]
-            }
+            $count: "totalLikes" // How is this working
         }
-    ])
-    
+    ]);
+
+    const totalSubscribedTo = await Subscription.countDocuments({ subscriber: channelId });
+
+    totalViews = totalViews[0]?.totalViews || 0;
+    totalLikes = totalLikes[0]?.totalLikes || 0;
+
+    return res.status(200)
+        .json(new ApiResponse(
+            200,
+            {
+                totalSubscribers,
+                totalSubscribedTo,
+                totalViews,
+                totalLikes,
+                totalVideos
+            },
+            "Channel stats fetched successfully"
+        ))
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
